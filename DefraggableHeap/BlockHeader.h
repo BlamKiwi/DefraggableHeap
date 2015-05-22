563f490687a12773d46789ef4632d7794ce6ccfe
Robbie Litchfield <robbie.l@missingbox.co.nz>
2015-05-22 23:28:45 +1200

    Fixed bad comments

--- DefraggableHeap/BlockHeader.h
+++ DefraggableHeap/BlockHeader.h
@@ -32,19 +32,19 @@ struct BlockHeader
 
 	/**
 	*	Constructs a block header from the given block data.
-	*	Does not calculate statistics for the heap tree.
+	*	Does not calculate statistics for the heap.
 	*
-	*	@param left the index for our left subtree
-	*	@param right the index for our right subtree
+	*	@param left the index for our left heap
+	*	@param right the index for our right heap
 	*	@param num_chunks the number of chunks on the block we represent
 	*	@param alloc the allocation state of the block
 	*/
 	BlockHeader(IndexType left, IndexType right, IndexType num_chunks, AllocationState alloc);
 
-	/**< Index of the left subtree for this header. */
+	/**< Index of the left heap for this header. */
 	IndexType _left;
 
-	/**< Index of the right subtree for this header. */
+	/**< Index of the right heap for this header. */
 	IndexType _right;
 
 	/**< Block allocation metadata. */
@@ -63,7 +63,7 @@ struct BlockHeader
 
 	} _block_metadata;
 
-	/**< The local maximum number of contiguous free chunks in the heap tree. */
+	/**< The local maximum number of contiguous free chunks in the heap. */
 	IndexType _max_contiguous_free_chunks;
 
 	static_assert(sizeof(_block_metadata) == sizeof(IndexType), "The metadata field must be the size of the index type.");
